f2f9bd759ed1b7dc9531f3a4e86917784a932bbd
tony dugué <duguetony@gmail.com>
2022-07-25 23:12:05 +0200

    add dependencies (tailwind & styled-components) & assets files

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
 
 export default App;
+
