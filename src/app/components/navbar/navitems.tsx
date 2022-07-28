2ac50488bbb89edd6c5671543ee952cb437d2a68
tony dugué <duguetony@gmail.com>
2022-07-28 18:58:07 +0200

    Homepage => add mobile menu (with react-burger-menu)

--- src/app/components/navbar/navitems.tsx
+++ src/app/components/navbar/navitems.tsx
@@ -1,22 +1,33 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 import tw from "twin.macro";
+import { slide as Menu } from 'react-burger-menu'
+import { useMediaQuery } from "react-responsive";
+import {SCREENS} from "../responsive";
+import menuStyles from "./menuStyles";
 
 const NavItems = () => {
+
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm })
+
+  if (isMobile)
+    return (
+      <Menu right styles={menuStyles}>
+        <ListContainer>
+          <NavItem menu><a href="">Home</a></NavItem>
+          <NavItem menu><a href="">Cars</a></NavItem>
+          <NavItem menu><a href="">Services</a></NavItem>
+          <NavItem menu><a href="">Contact Us</a></NavItem>
+        </ListContainer>
+      </Menu>
+    )
+
   return (
    <ListContainer>
-    <NavItem>
-      <a href="">Home</a>
-    </NavItem>
-     <NavItem>
-       <a href="">Cars</a>
-     </NavItem>
-     <NavItem>
-       <a href="">Services</a>
-     </NavItem>
-     <NavItem>
-       <a href="">Contact Us</a>
-     </NavItem>
+    <NavItem><a href="">Home</a></NavItem>
+     <NavItem><a href="">Cars</a></NavItem>
+     <NavItem><a href="">Services</a></NavItem>
+     <NavItem><a href="">Contact Us</a></NavItem>
    </ListContainer>
   )
 }
@@ -30,7 +41,7 @@ const ListContainer = styled.ul`
     `}
 `
 
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any}>`
   ${tw`
     text-xs 
     md:text-base 
@@ -43,5 +54,9 @@ const NavItem = styled.li`
     duration-300
     ease-in-out
     hover:text-gray-700
+  `}
+
+  ${({ menu }) => menu && css`
+    ${tw`text-white text-xl mb-3 focus:text-white`}
   `}
 `
